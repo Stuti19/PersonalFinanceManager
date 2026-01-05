c4dbc4ed94a8615edfeb5a591285cf0b8225d405
Stuti19 <stutib19@gmail.com>
2026-01-05 16:33:05 +0530

    Fix JAVA_HOME detection in build script

--- build.sh
+++ build.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-# Install OpenJDK 17
-apt-get update
-apt-get install -y openjdk-17-jdk
+set -e
 
-# Set JAVA_HOME
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
+# Install OpenJDK 17
+sudo apt-get update
+sudo apt-get install -y openjdk-17-jdk
+
+# Find and set JAVA_HOME
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+echo "JAVA_HOME set to: $JAVA_HOME"
+
+# Verify Java installation
+java -version
+javac -version
 
 # Install Maven
-curl -fsSL https://archive.apache.org/dist/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz | tar xz
+wget https://archive.apache.org/dist/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz
+tar xzf apache-maven-3.9.4-bin.tar.gz
 export PATH=$PWD/apache-maven-3.9.4/bin:$PATH
+
+# Verify Maven installation
+mvn -version
 
 # Build project
 mvn clean package -DskipTests
